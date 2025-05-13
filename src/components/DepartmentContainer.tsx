ffe55c81207d974750ea56be789cbff6cd0c81da
George Ulloa <gulloa@feam.aero>
2025-05-13 11:44:53 -0400

    new button

--- src/components/DepartmentContainer.tsx
+++ src/components/DepartmentContainer.tsx
@@ -18,17 +18,13 @@ const DepartmentContainer: React.FC<DepartmentContainerProps> = ({
   departmentDescription,
 }) => {
   return (
-    <div>
-      
-      <div className='mainbox'>
-          <div className='imagebox'>
-            <img src={imagePath} alt={alt} className='department-image' />
-          </div>
-          <div className='textbox'>
-            <h2 className='department-name'>{departmentName}</h2>
-            <p className='department-description'>{departmentDescription}</p>
-          </div>
-
+    <div className='mainbox'>
+      <div className='imagebox'>
+        <img src={imagePath} alt={alt} className='department-image' />
+      </div>
+      <div className='textbox'>
+        <h2 className='department-name'>{departmentName}</h2>
+        <p className='department-description'>{departmentDescription}</p>
       </div>
     </div>      
   )
